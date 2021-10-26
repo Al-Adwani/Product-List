46d3ff1ea8a46e1b717389d13fb03f182b0b5e30
Abdullah Al Adwani <abdullahmmaladwani@gmail.com>
2021-10-26 18:32:15 +0300

    git ignore Done

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   image: String,
   description: String,
   color: String,
-  quantity: Number,
-  price: Number,
+  quantity: { type: Number, min: 0 },
+  price: { type: Number, values: 12 },
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
